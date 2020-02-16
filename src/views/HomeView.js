5525b29ab437873ff2d8468039f9b133a2ca3a87
github username <josephtrettevik@gmail.com>
2020-02-16 11:44:28 -0800

    added a css file for the homepage and rounded the borders of my profile picture.

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react'
 import Photo from '../images/profilePicture.jpg'
+import '../HomeView.css'
 class HomeView extends Component {
 render() {
     return (
