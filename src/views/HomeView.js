64753057e63842c3e61a0f48b2692979cca392bf
github username <josephtrettevik@gmail.com>
2020-02-23 15:43:17 -0800

    added info my geocaching and tasker apps and passed that info into two new ProjectCards

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -12,6 +12,16 @@ projectInfo = {
         'title': 'Bigger Fish',
         'stack': 'React.js|JavaScript|Ruby on Rails|Semantic-UI',
         'description': 'This app using google maps tools to match volunteer delivery drivers with donors that want to donate parishable food.'
+    },
+    'geocachingApp': {
+        'title': 'Geocaching App',
+        'stack': 'React.js|JavaScript|Ruby on Rails|Semantic-UI',
+        'description': 'Displays nearby geocaches and allows users to create their own.'
+    },
+    'tasker': {
+        'title': 'Tasker',
+        'stack': 'JavaScript|Ruby on Rails|CSS',
+        'description': 'A time management app that allows users to add tasks to "To Do," "Working," or "Done," and move them between those lists.'
     }
 }
 
@@ -24,6 +34,8 @@ render() {
             <br/>
             <p>Full stack software engineer with a passion for solving puzzles. Experience in React, JavaScript, and Ruby on Rails, and strong skills in problem solving and writing algorithms that help tech companies develop new product features. From my work as a Marine Chemist, I know how to troubleshoot problems when you can’t directly see a system’s inner workings, and I use this in my approach to software development. </p>
             <ProjectCard cardInfo={this.projectInfo.biggerFish}/>
+            <ProjectCard cardInfo={this.projectInfo.geocashingApp}/>
+            <ProjectCard cardInfo={this.projectInfo.tasker}/>
         </Fragment>
     );
 }
