fc3a27841dc7592f1a0ffa6b0066fc2580d80d38
github username <josephtrettevik@gmail.com>
2020-02-23 14:32:58 -0800

    created a project card container file

---
+++ src/containers/ProjectCard.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react'
+
+class ProjectCard extends Component {
+
+    constructor() {
+
+    }
+
+    render() {
+        return (
+            <h1>This in my ProjectCard</h1>
+        );
+    }
+
+}
+
+export default ProjectCard
