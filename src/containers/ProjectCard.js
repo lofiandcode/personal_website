b48767197a641b223d26ff2cebc651e3cffdb15d
github username <josephtrettevik@gmail.com>
2020-02-23 14:58:05 -0800

    installed semantic-ui-react and used it to set up bare bones project card

--- src/containers/ProjectCard.js
+++ src/containers/ProjectCard.js
@@ -1,17 +1,24 @@
-import React, {Component, Fragment} from 'react'
+import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+import Photo from '../images/profilePicture.jpg'
 
-class ProjectCard extends Component {
-
-    constructor() {
-
-    }
-
-    render() {
-        return (
-            <h1>This in my ProjectCard</h1>
-        );
-    }
-
-}
+const ProjectCard = () => (
+  <Card>
+    <Image src={Photo} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Daniel</Card.Header>
+      <Card.Meta>Joined in 2016</Card.Meta>
+      <Card.Description>
+        Daniel is a comedian living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        10 Friends
+      </a>
+    </Card.Content>
+  </Card>
+)
 
 export default ProjectCard
