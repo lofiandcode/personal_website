8fd42ce164e28cd59166b4099693ea9f941b21ef
github username <josephtrettevik@gmail.com>
2020-02-23 15:33:01 -0800

    updated ProjectCard so that all of the fields are filled with content from props

--- src/containers/ProjectCard.js
+++ src/containers/ProjectCard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
-const ProjectCard = () => (
+const ProjectCard = (props) => (
   <Card>
     <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
     <Card.Content>
-      <Card.Header>Bigger Fish</Card.Header>
-      <Card.Meta>React.js|JavaScript|Ruby on Rails|Semantic-UI </Card.Meta>
+      <Card.Header>{props.cardInfo.title}</Card.Header>
+      <Card.Meta>{props.cardInfo.stack}</Card.Meta>
       <Card.Description>
-        This app using google maps tools to match volunteer delivery drivers with donors that want to donate parishable food.
+        {props.cardInfo.description}
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -23,7 +23,7 @@ render() {
             <p>Full Stack Software Engineer / React.js / JavaScript / Ruby on Rails</p>
             <br/>
             <p>Full stack software engineer with a passion for solving puzzles. Experience in React, JavaScript, and Ruby on Rails, and strong skills in problem solving and writing algorithms that help tech companies develop new product features. From my work as a Marine Chemist, I know how to troubleshoot problems when you can’t directly see a system’s inner workings, and I use this in my approach to software development. </p>
-            <ProjectCard/>
+            <ProjectCard cardInfo={this.projectInfo.biggerFish}/>
         </Fragment>
     );
 }
